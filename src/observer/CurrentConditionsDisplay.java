67e41297fa32549e8db47ddc5357a6ecb8843ee9
rosspalmer <rossbpalmer@gmail.com>
2017-11-05 16:16:51 -0700

    adding Observer pattern examples

---
+++ src/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,30 @@
+package observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+    private Observable observable;
+    private float temperature;
+    private float humidity;
+
+    public CurrentConditionsDisplay(Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
+    }
+
+    public void update(Observable obs, Object arg) {
+        if (obs instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) obs;
+            temperature = weatherData.getTemperature();
+            humidity = weatherData.getHumidity();
+            display();
+        }
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+
+}
